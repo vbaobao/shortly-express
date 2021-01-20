8c9d120a90421588275ed08ff5ade97fbccf1e35
Shane Sutro <shanesutro@gmail.com>
2021-01-19 19:21:03 -0700

    Complete Parsing Cookie

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const partials = require('express-partials');
 const bodyParser = require('body-parser');
 const Auth = require('./middleware/auth');
 const models = require('./models');
+const cookieParser = require('./middleware/cookieParser');
 
 const app = express();
 
@@ -14,6 +15,8 @@ app.use(partials());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(cookieParser);
 
 app.get('/', (req, res) => {
   res.render('index');
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,17 @@
 const parseCookies = (req, res, next) => {
+  // access the cookies on an incoming request header
+  let cookies = req.headers.cookie
+    ? (req.headers.cookie).split('; ')
+    : [];
+  let parsedCookies = {};
+  // parse them into an object
+  for (cookie of cookies) {
+    let temp = cookie.split('=');
+    parsedCookies[temp[0]] = temp[1];
+  }
+  // assign this object to a cookies property on the req
+  req.cookies = parsedCookies;
+  next();
 };
 
 module.exports = parseCookies;
